1753f51c4286fe9b6a3c491b404bfac116f74d75
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-04 16:33:50 +0530

    basic server config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const express = require("express");
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000 ;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    console.log('goals');
+    res.status(201).send('goals');
+    
+});
+
+app.listen(port, () => {
+  console.log(`server is listning to port: ${port}`);
+});
