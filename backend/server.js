3a9a06f6265eaad7c5cf29de4a2858c82d478a1b
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-04 16:47:53 +0530

    create basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000 ;
+const goalRoutes = require('./routes/goalRoutes');
 
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-    console.log('goals');
-    res.status(201).send('goals');
-    
-});
+app.use('/api/goals', goalRoutes);
 
 app.listen(port, () => {
   console.log(`server is listning to port: ${port}`);
