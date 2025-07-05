1a61ef79dd03ab26624bf718d20be1b1c1f1b0b5
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-05 15:41:48 +0530

    add user to goal model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: true,
     },
   },
   {
-    timestamps: true,   //the timestamps: true option automatically adds two fields to the schema: createdAt and updatedAt. These fields are used to track when a document is created and last updated.
+    timestamps: true, //the timestamps: true option automatically adds two fields to the schema: createdAt and updatedAt. These fields are used to track when a document is created and last updated.
   }
 );
 
-module.exports = mongoose.model('Goal', goalSchema);
+module.exports = mongoose.model("Goal", goalSchema);
